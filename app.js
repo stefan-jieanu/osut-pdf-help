8f94f0cdb3849cc2a6f2d043fd98d1e6bfea021c
StefanJ00 <stefan.jieanu00@e-uvt.ro>
2021-02-27 12:41:56 +0200

    Basic python script exectuing

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express = require('express');
+const {spawn} = require('child_process');   
 const server = express();
 
 const port = 3000;
 
 server.get('/', (req, res) => {
-    res.send('hello world');
+    let python_output;
+
+    // spawn a process to run the python script
+    const python_script = spawn('python', ['example.py']);
+
+    // collect the data from script
+    python_script.stdout.on('data', (data) => {
+        python_output = data.toString();
+        console.log('running script');
+    });
+
+    // close event to make sure sure the child process is closed
+    python_script.on('close', (code) => {
+        console.log('Python closed with code: ', code);
+        console.log(python_output);
+    });
+
+    // send the python output to the browser
+    res.send(python_output);
 });
 
 server.listen(port, () => {
